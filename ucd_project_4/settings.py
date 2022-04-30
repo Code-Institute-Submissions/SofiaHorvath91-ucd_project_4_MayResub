61a16b25fced561a8c5df3a713ab737c835cf3b3
Zsofia Horvath <horvathzsofia9105@gmail.com>
2022-04-30 15:51:50 +0100

    Deploy heroku testing

--- ucd_project_4/settings.py
+++ ucd_project_4/settings.py
@@ -162,10 +162,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-#STATICFILES_FINDERS = (
-#    'django.contrib.staticfiles.finders.FileSystemFinder',
-#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
